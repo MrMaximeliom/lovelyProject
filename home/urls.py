1cc3f972004b75ca72e47ea0f404bc6a2704b0d3
Moayed <moayedEissa@outlook.sa>
2020-06-24 20:37:47 +0200

    first commit

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+# this is my changes
 urlpatterns = [
     path('', views.home, name='home-page'),
     path('home/', views.home, name='home-page'),
